882c7f2144a011d9e093663ff076b49db2452ec4
Haytam El Ouarrat <eo.haytam@gmail.com>
2023-04-01 19:15:27 +0200

    commit

--- MainGraphique.java
+++ MainGraphique.java
@@ -1,14 +1,49 @@
 package ProjetFilRouge;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class MainGraphique {
 
-    public MainGraphique(){
+    public static void main(String[] args) {
+        // Read in X, Y and the values from the file
+        int xSize = 0;
+        int ySize = 0;
+        int[][] values = null;
+        try (Scanner scanner = new Scanner(new File("C:\\Users\\eohay\\Documents\\PFRG7\\src\\ProjetFilRouge\\TEST_NB\\63.txt"))) {
+            xSize = scanner.nextInt();
+            ySize = scanner.nextInt();
+            values = new int[xSize][ySize];
+            for (int y = 0; y < ySize; y++) {
+                for (int x = 0; x < xSize; x++) {
+                    values[x][y] = scanner.nextInt();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-    }
-    public static void main(String[] args) throws IOException, FontFormatException {
-        
+        // Create a BufferedImage with the same dimensions as the input values
+        BufferedImage image = new BufferedImage(xSize, ySize, BufferedImage.TYPE_INT_RGB);
+
+        // Iterate through the values and set each pixel color accordingly
+        for (int y = 0; y < ySize; y++) {
+            for (int x = 0; x < xSize; x++) {
+                int value = values[x][y];
+                Color color = new Color(value, value, value);
+                image.setRGB(x, y, color.getRGB());
+            }
+        }
+
+        // Write the image to file
+        try {
+            ImageIO.write(image, "png", new File("output.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- vuegraphique/PanRechercher.java
+++ vuegraphique/PanRechercher.java
@@ -42,11 +42,11 @@ public class PanRechercher extends JPanel {
     JList<String> list;
     JSplitPane splitPane;
     TextArea textArea;
-    ImageIcon imageIcon;
-    JScrollPane picScrollPane;
 
+    JPanel panelRightComponent;
+
+    JLabel image;
     private JLabel titre = new JLabel("Resultats de la recherche");
-    private JLabel image;
     // Mise en page : les Box
     Box boxMiseEnPageRecherche = Box.createVerticalBox();
     Box boxMiseEnPageResultat = Box.createVerticalBox();
@@ -62,13 +62,14 @@ public class PanRechercher extends JPanel {
     }
 
     //Methode d'initialisation du panel
-    public void initialisation() {
+    public void initialisation() throws IOException {
         recherche = rechercher(typeRecherche);
         resultats = controlResultat.getCheminsResultats(recherche);
         // mise en forme du panel (couleur, ...)
         setBackground(Color.RED);
         // creation des differents elements graphiques (JLabel, Combobox, Button, TextAera ...)
         JLabel titreRes = new JLabel("Resultats de la recherche");
+        panelRightComponent = new JPanel();
         titre.setFont(policeTitre);
         list = new JList<>(resultats);
         list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
@@ -81,42 +82,28 @@ public class PanRechercher extends JPanel {
                 String selected = source.getSelectedValue();
                 try {
                     textArea.setText(ControlFichier.readFileAsString(selected));
-                    updateLabel(resultats[list.getSelectedIndex()]);
                 } catch (IOException ex) {
                     throw new RuntimeException(ex);
                 };
             }
         });
-        String choice = list.getSelectedValue();
         //textArea
         textArea = new TextArea();
         textArea.setEditable(false);
         textArea.setFont(policeParagraphe);
-        //imageViewer
-        picScrollPane = new JScrollPane(image);
-        Dimension minimumSize = new Dimension(100, 50);
-        picScrollPane.setMinimumSize(minimumSize);
-
-        image = new JLabel();
-        image.setFont(image.getFont().deriveFont(Font.ITALIC));
-        image.setHorizontalAlignment(JLabel.CENTER);
+        //image
+        image = new JLabel(new ImageIcon(ControlFichier.getCheminRelative()+TypeFichier.getRepertoireResultatFromExtension(ControlFichier.getFileExtension(list.getSelectedValue()))));
         //splitPane
-        //splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, list, textArea);
-        splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, list, switch (Objects.requireNonNull(TypeFichier.getTypeFromExtension(ControlFichier.getFileExtension(list.getSelectedValue())))){
-
-            case TEXTE -> textArea;
-            case IMAGE -> picScrollPane;
-            case AUDIO -> null;
-        });
+        splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, list, textArea);
         splitPane.setOneTouchExpandable(true);
         splitPane.setDividerLocation(150);
-        splitPane.setPreferredSize(new Dimension(400, 200));
+
         //boutons
         boutonOuvrir.setText("Ouvrir");
         boutonOuvrir.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                ControlFichier.ouvrirFichier(choice);
+                ControlFichier.ouvrirFichier(list.getSelectedValue());
             }
         });
         boutonRetour.setText("Retour");
@@ -147,29 +134,6 @@ public class PanRechercher extends JPanel {
                 return controlRecherche.rechercherSon(cheminFichier);
             default:
                 return null;
-        }
-    }
-    public void valueChanged(ListSelectionEvent e) {
-        JList<String> list = (JList)e.getSource();
-        updateLabel(resultats[list.getSelectedIndex()]);
-    }
-    protected void updateLabel (String path) {
-        ImageIcon icon = createImageIcon(path);
-        image.setIcon(icon);
-        if  (icon != null) {
-            image.setText(null);
-        } else {
-            image.setText("Image not found");
-        }
-    }
-    protected static ImageIcon createImageIcon(String path) {
-        path = ControlFichier.getCheminRelative()+TypeFichier.getRepertoireResultatFromExtension(ControlFichier.getFileExtension(path))+path;
-        ImageIcon im = new ImageIcon(path);
-        if (im != null) {
-            return new ImageIcon(path);
-        } else {
-            System.err.println("Couldn't find file: " + path);
-            return null;
         }
     }
 }
